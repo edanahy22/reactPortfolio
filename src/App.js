eea5910f6aa80bc30250f1df44c215706d7b27c5
Elaine Danahy <edanahy22@gmail.com>
2022-08-29 21:26:59 -0500

    added router and routing to different pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
+//react
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+//components
+import Header from './components/Header';
+// import Navigation from './components/Navigation';
+// import Project from './components/Project';
+// import Footer from './components/Footer';
+
+//pages
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Portfolio from './components/pages/Portfolio';
+import Resume from './components/pages/Resume';
+
+// styling
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-import Header from './components/Header'
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header>
         <Header/>
       </header>
+      <body>
+        <Switch>
+          <Route exact path="/">
+            <About/>
+          </Route>
+          <Route path="/Resume">
+            <Resume/>
+          </Route>
+          <Route path='/Portfolio'>
+            <Portfolio/>
+          </Route>
+          <Route path="/Contact">
+            <Contact/>
+          </Route>
+        </Switch>
+      </body>
     </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-md bg-light">  
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Elaine Danahy</a>
+        <h1 className="navbar-brand">Elaine Danahy</h1>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">                
           <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <a className="nav-link" href="#">Home</a>
-            <a className="nav-link" href="#">Projects</a>
-            <a className="nav-link" href="#">Contact</a>
-            <a className="nav-link" href="#">Resume</a>
-          </div>
+          <ul className="navbar-nav">
+            <Link to="/">
+            <li className="nav-link"> About</li>
+            </Link>
+            <Link to="/Portfolio">
+            <li className="nav-link"> Portfolio</li>
+            </Link>
+            <Link to="/Contact">
+            <li className="nav-link">Contact</li>
+            </Link>
+            <Link to="/Resume">
+            <li className="nav-link">Resume</li>
+            </Link>
+          </ul>
         </div>
       </div> 
     </nav>
