ba10fa049f231431b2dfa114bc93b8acb59f21cf
Elaine Danahy <edanahy22@gmail.com>
2022-08-29 21:47:25 -0500

    added github pages dep

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,9 +12,9 @@ const Header = () => {
         </button>
 
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <ul className="navbar-nav">
-            <Link to="/">
-            <li className="nav-link"> About</li>
+          <ul className="navbar-nav" style={{ textDecoration: 'none' }}>
+            <Link to="/" style={{ textDecoration: 'none' }}>
+            <li className="nav-link" style={{ textDecoration: 'none' }} > About</li>
             </Link>
             <Link to="/Portfolio">
             <li className="nav-link"> Portfolio</li>
